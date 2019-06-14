c8abd6bf5d8259414f995e539ba2e6715ad4f934
Arslan Khaliq <akhaliq@gtek.solutions>
2019-06-15 01:03:35 +0500

    Initial Commit

---
+++ Acty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Acty
+//
+//  Created by Javeed Ishaq on 15/06/2019.
+//  Copyright © 2019 Javeed Ishaq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
