8bd4c8cb31ec98cfa07d228c2a7d07bdca29be95
Arslan Khaliq <akhaliq@gtek.solutions>
2019-06-15 12:29:16 +0500

    completed App

--- Acty/ViewController.swift
+++ Acty/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var emotions = ["happy", "sad", "excited", "concerned", "surprised", "trusting", "worried", "in anticipation", "disappointed", "surprised", "frustrated"]
+    
+    var activities = ["swimming with dolphins", "planting trees", "sailing in the ocean", "reading a book", "riding a horse", "watching a movie", "riding a bike", "cleaning your house", "dining with friends", "running from a lion", "crawling in a desert"]
+    
+    
 
+    @IBOutlet weak var actingTask: UILabel!
+    
+    @IBAction func generateActingTask() {
+        //generate random emotion
+        let randomEmotionIndex = Int(arc4random_uniform(UInt32(emotions.count)))
+        let randomEmotion = emotions[randomEmotionIndex]
+        
+        //generate random activity
+        let randomActivityIndex = Int(arc4random_uniform(UInt32(activities.count)))
+        let randomActivity = activities[randomActivityIndex]
+        
+        //modify the label
+        let newActingTask = "You are " + randomEmotion + " " + randomActivity + "!"
+        actingTask.text = newActingTask
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
 
 
